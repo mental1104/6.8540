50ec791762b013d4c7f654d4511caa4cd8cff177
mental1104 <mental1104@gmail.com>
2024-10-27 15:39:13 +0800

    [ADD] 基本数据结构定义 + coordinator编写完成

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,6 +1,7 @@
 package mr
 
 import "fmt"
+import "time"
 import "log"
 import "net/rpc"
 import "hash/fnv"
@@ -25,16 +26,91 @@ func ihash(key string) int {
 }
 
 
+type MsgType int
+
+const (
+	AskForTask   MsgType = iota    // Worker -> Coordinator 请求任务
+	MapTaskAlloc                   // Coordinator -> Worker 分配Map任务
+	ReduceTaskAlloc                // Coordinator -> Worker 分配Reduce任务
+	MapSuccess                     // Worker -> Coordinator Map任务执行成功
+	MapFailed                      // Worker -> Coordinator Map任务执行失败
+	ReduceSuccess                  // Worker -> Coordinator Reduce任务执行成功
+	ReduceFailed                   // Worker -> Coordinator Reduce任务执行失败
+	Shutdown                       // Coordinator -> Worker 没有任务了，可以退出了
+	Wait                           // Coordinator -> Worker 当前暂时没有任务分配，需要等待
+)
+
+
+type MessageSend struct {
+	MsgType MsgType    // RPC 消息类型
+	TaskId int         // 调用NoticeResult时需要指定具体成功/失败的任务ID
+}
+
+
+type MessageReply struct {
+	MsgType MsgType  // RPC 消息类型
+	NReduce int      // Map任务中，worker需要向coordinator确认reduce结果的数量
+	TaskId  int      // 任务ID选取输入文件
+	TaskName string  // Map任务专用，Coordinator告知Worker具体该打开哪个文件
+}
+
+
+func CallForTasks() *MessageReply {
+    args := MessageSend{
+		MsgType: AskForTask,
+	}
+
+	reply := MessageReply{}
+
+	ret := call("Coordinator.AskForTask", &args, &reply)
+	if ret {
+		return &reply
+	} else {
+		return nil
+	}
+}
+
+
+func CallForReportStatus(successType MsgType, taskID int) bool {
+	args := MessageSend{
+		MsgType: successType,
+		TaskId:  taskID,
+	}
+
+	ret := call("Coordinator.NoticeResult", &args, nil)
+
+	return ret
+}
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
-	// Your worker implementation here.
+	for {
+		time.Sleep(1 * time.Second)
+		replyMsg := CallForTasks()
+		if replyMsg == nil {
+			fmt.Println("reply failed!, continue")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		switch replyMsg.MsgType {
+		case MapTaskAlloc:
+			fmt.Println("MapTaskAlloc")
+		case ReduceTaskAlloc:
+			fmt.Println("ReduceTaskAlloc")
+		case Shutdown:
+			fmt.Println("Task completed, Shutdown")
+		case Wait:
+			fmt.Println("Task blocked, wait")
+		default:
+			fmt.Printf("Not yet supported type: %d\n", replyMsg.MsgType)
+			time.Sleep(1 * time.Second)
+		}
 
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+
+	}
 
 }
 
